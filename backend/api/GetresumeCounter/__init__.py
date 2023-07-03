6c2f60079933289e1fcd8d20e7303fddb52f11be
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2023-07-03 15:18:46 +0530

    code:added functionapi link

--- backend/api/GetresumeCounter/__init__.py
+++ backend/api/GetresumeCounter/__init__.py
@@ -1,24 +1,31 @@
 import logging
-
+import os
+from azure.data.tables import TableServiceClient, UpdateMode
 import azure.functions as func
+import json
 
 
+conn= os.getenv("AzureResumeConnectionString")
+service = TableServiceClient.from_connection_string(conn_str=conn)
+table_client = service.get_table_client(table_name="counter")
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    my_filter = "PartitionKey eq '1' and RowKey eq '1'"
+    entities = table_client.query_entities(my_filter)
+    for entity in entities:
+        for key in entity.keys():
+            if key == "count":
+                updatedcounter = entity[key]
+                entity[key]= updatedcounter+1
+                table_client.update_entity(mode=UpdateMode.MERGE, entity=entity)
+                response = {
+                    "key": "count",
+                    "value": updatedcounter
+                }
+            
+    return func.HttpResponse(
+        body=json.dumps(response),
+        status_code=200,
+        mimetype='application/json'
+    )
+        # return("Key: {}, Value: {}".format(key, entity[key]))
--- frontend/main.js
+++ frontend/main.js
@@ -1,13 +1,13 @@
 window.addEventListener('DOMContentLoaded',(event)=>{getVisitcount();})
-const functionApi ='';
+const functionApi ="http://localhost:7071/api/GetresumeCounterhttps://resumecounterazure.azurewebsites.net/api/GetresumeCounter?code=7Esq0IaYSll_a_raBAsztfB868gwWAmlgBxqeEVhTMsdAzFuNF_F0Q==";
 
 const getVisitcount =()=>{
-    let count =1;
+    let count =0;
     fetch(functionApi).then(response=>{
-        return response.json
+        return response.json()
     }).then(response=>{
         console.log("The site called the function api")
-        count = response.count;
+        count = response.value;
         document.getElementById("counter").innerText =count;
     }).catch(function(error){
         console.log(error);
